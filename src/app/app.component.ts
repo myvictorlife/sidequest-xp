00fc9b1b1314a6b3e44ef7ec42804865ed7b4a63
Victor Cesar <victorcesar@Victors-MacBook-Pro.local>
2022-05-03 07:41:06 +0200

    feat: add ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'sidequest-xp-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sidequest-xp';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'pt']);
+    translate.setDefaultLang('en');
+  }
 }
