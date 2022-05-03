bc921df5bf2fcfa33535f0cacd967c8013754037
Victor Cesar <victorcesar@Victors-MacBook-Pro.local>
2022-05-03 09:21:27 +0200

    feat: add html version

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import packageJson from '../package.json';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+document.querySelector('html')?.setAttribute('sidequest-xp-version', packageJson.version);
